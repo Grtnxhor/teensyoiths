88468015e3b92d8c9e836ec4401a6c4af545539c
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-06-22 22:35:46 +0100

    done

--- dashboard/article.php
+++ dashboard/article.php
@@ -61,7 +61,7 @@ $row = mysqli_fetch_array($res);
                          <tr>
                              <?php
 $author = $_SESSION['Username'];             
- $sql="SELECT * from `article` WHERE `author` = '$author' ORDER BY 'id' desc";
+ $sql="SELECT * from `article` ORDER BY 'id' desc WHERE `author` = '$author'";
  $result_set = query($sql);
  while($row = mysqli_fetch_array($result_set))
  {
